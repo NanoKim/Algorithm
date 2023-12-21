d20330fcd788eb34a14431cedcc17e2a55bf4e4b
Kim JaeYeong <kjyyy7341@gmail.com>
2023-12-22 02:57:59 +0900

    [Silver V] Title: 소트인사이드, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		char[] ch = s.toCharArray();
+		
+		Arrays.sort(ch);
+		
+		for(int i = ch.length-1; i >= 0; i--) {
+			System.out.print(ch[i]);
+		}
+		
+	}
+
+}
