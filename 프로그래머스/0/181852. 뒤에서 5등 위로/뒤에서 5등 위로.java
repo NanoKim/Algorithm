3d15fe8e1662b2ecbd621f5af984ece0a57ff54d
Jaeyeong Kim <kjyyy7341@gmail.com>
2024-02-17 11:19:37 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.38 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] arr = new int[num_list.length - 5];
+        Arrays.sort(num_list);
+        
+        for (int i = 5; i < num_list.length; i++) {
+            arr[i - 5] = num_list[i];
+        }
+        
+        return arr;
+    }
+}
