a83c0d94a1af06f85368ba920caea5ae6777affc
Jaeyeong Kim <kjyyy7341@gmail.com>
2024-02-26 06:03:45 +0900

    [Bronze II] Title: 분해합, Time: 236 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	Scanner sc = new Scanner(System.in);
+
+    	int n = sc.nextInt();
+    	int answer = 0;
+    	
+    	for(int i = 0; i < n; i++) {
+    		int num = i;
+            int sum = i;
+
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum == n){
+                answer = i;
+                break;
+            }
+    	}
+    	System.out.println(answer);
+    }    
+}
