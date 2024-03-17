b999d3d55551055bf413e57e3916dfb231c58914
Jaeyeong Kim <kjyyy7341@gmail.com>
2024-03-18 01:00:21 +0900

    [Silver IV] Title: solved.ac, Time: 352 ms, Memory: 37528 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int trimmedNum = (int) Math.round((float) N * 0.15);
+
+		int[] solved = new int[N];
+
+		for(int i = 0; i < N; i ++){
+			solved[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(solved);
+
+		float sum = 0;
+
+		for(int i = trimmedNum; i < N - trimmedNum; i ++){
+			sum += solved[i];
+		}
+
+		System.out.println( Math.round(sum / (float) (N - trimmedNum * 2) ));
+
+	}
+
+}
